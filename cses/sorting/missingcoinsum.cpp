73c91c87d873bfd558b5b7de38a296a07c8403ab
s-sharar <ssharar@uwaterloo.ca>
2025-12-29 18:25:45 -0500

    fix: fix missing sum sol

--- cses/sorting/missingcoinsum.cpp
+++ cses/sorting/missingcoinsum.cpp
@@ -4,13 +4,13 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+
     int n;
     cin >> n;
-    vector<int> a(n);
-    for (int i = 0; i < n; ++i) {
-        cin >> a[i];
-    }
+    vector<long long> a(n);
+    for (int i = 0; i < n; ++i) cin >> a[i];
 
+    sort(a.begin(), a.end());  // <-- required
 
     long long reach = 0;
     for (long long x : a) {
